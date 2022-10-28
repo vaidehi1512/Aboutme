eb47adfca997c89ae3db5e0992ffcdddf5be68a9
vaidehi1512 <107156300+vaidehi1512@users.noreply.github.com>
2022-10-27 22:14:47 -0700

    Add files via upload

---
+++ src/Pages/ContactPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function ContactPage() {
+    return (
+        <div className="contactPage">
+            <h1>Get In Touch</h1>
+              Contact Page
+        </div>
+    )
+}
+
+export default ContactPage
